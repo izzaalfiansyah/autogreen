f757e153da33d0d5ca8d6074a6650a1d7a3de1d5
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-06-28 15:10:39 +0700

    generate diagram

---
+++ src/components/images.tsx
@@ -0,0 +1,6 @@
+export default {
+  tandon: "https://cdn-icons-png.flaticon.com/512/2399/2399504.png",
+  pompa: "https://cdn-icons-png.flaticon.com/512/2226/2226103.png",
+  katup: "https://cdn-icons-png.flaticon.com/512/148/148298.png",
+  sensor: "https://cdn-icons-png.flaticon.com/512/6375/6375838.png",
+};
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -5,13 +5,15 @@ export default component$(() => {
   useStyles$(styles);
 
   return (
-    <div class="min-h-screen bg-gray-50 diagram">
-      <div class="flex h-20 items-center justify-between bg-white shadow-sm lg:px-10 px-5">
+    <div class="min-h-screen bg-gray-50 flex flex-col">
+      <div class="flex h-20 items-center justify-between bg-white shadow-sm lg:px-10 px-5 sticky top-0 left-0 right-0">
         <div>
-          <span class="font-semibold text-lg">Autogreen</span>
+          <span class="font-semibold text-lg">
+            <span class="text-green-500">AI </span>Green
+          </span>
         </div>
       </div>
-      <div class="lg:p-10 p-5">
+      <div class="grow diagram flex flex-col">
         <Slot />
       </div>
     </div>
