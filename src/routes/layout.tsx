058a8057320203347fbec29df93aa7feef80b7b2
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-06-28 12:39:46 +0700

    make layout apge

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,38 +1,19 @@
-import { component$, Slot, useStyles$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
-import type { RequestHandler } from '@builder.io/qwik-city';
-
-import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
-
-import styles from './styles.css?inline';
-
-export const onGet: RequestHandler = async ({ cacheControl }) => {
-  // Control caching for this request for best performance and to reduce hosting costs:
-  // https://qwik.builder.io/docs/caching/
-  cacheControl({
-    // Always serve a cached response by default, up to a week stale
-    staleWhileRevalidate: 60 * 60 * 24 * 7,
-    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-    maxAge: 5,
-  });
-};
-
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
+import { Slot, component$, useStyles$ } from "@builder.io/qwik";
+import styles from "./styles.css?inline";
 
 export default component$(() => {
   useStyles$(styles);
+
   return (
-    <>
-      <Header />
-      <main>
+    <div class="min-h-screen bg-gray-50 diagram">
+      <div class="flex h-20 items-center justify-between bg-white shadow-sm lg:px-10 px-5">
+        <div>
+          <span class="font-semibold text-lg">Autogreen</span>
+        </div>
+      </div>
+      <div class="lg:p-10 p-5">
         <Slot />
-      </main>
-      <Footer />
-    </>
+      </div>
+    </div>
   );
 });
