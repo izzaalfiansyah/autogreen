2e011fbfd78bbdf894de3940e282642ed6532d85
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-07-05 10:12:59 +0700

    make simpler

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./src/**/*.{html,js,ts,jsx,tsx,mdx}', './index.html'],
   theme: {
     extend: {},
   },
